494fde4127d168b56e13edc02d7151031f874e04
arshad-k99 <arshadlfc99@gmail.com>
2022-06-15 20:49:06 +0530

    Model and Migration file added

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->Increments('id');
+            $table->string('Name');
+            $table->integer('Fk_department');
+            $table->integer('Fk_designation');
+            $table->integer('Phone_number');
+            $table->timestamp('Created_at');
+        });
+
+        
+
+
+         // Query Builder approach
+
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
